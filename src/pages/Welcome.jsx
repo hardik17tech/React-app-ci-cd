8a7dd90b25f73fb7f3bf9d70463bc2e42f30eae8
hardik17tech <hardik.pan@befree.com.au>
2026-02-18 12:27:03 +0530

    Update Welcome.jsx

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Welcome() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-blue-50">
-      <h1 className="text-4xl font-bold mb-4">Welcome to My App Login 🚀</h1>
+      <h1 className="text-4xl font-bold mb-4">Welcome to My App Login 🚀 123</h1>
       <p className="mb-6 text-gray-600">Click below to login and continue and move on the login page</p>
       <Link
         to="/login"
